09d91271316605614034b7dafb5505ea55b57d17
John Lafitte <john.lafitte@gmail.com>
2023-03-14 11:46:44 -0500

    Add google maps and platforms For testing

--- app/main-page.js
+++ app/main-page.js
@@ -1,6 +1,41 @@
 import { createViewModel } from './main-view-model';
+import { Dialogs } from "@nativescript/core";
 
 export function onNavigatingTo(args) {
-  const page = args.object
-  page.bindingContext = createViewModel()
+  const page = args.object;
+  page.bindingContext = createViewModel();
+}
+
+export function onReady(args) {
+  Dialogs.alert("Map ready.");
+  let map = args.map;
+  let polylineOptions = {
+    width: 3,
+    points: [
+      {
+        lat: 29,
+        lng: -95
+      },
+      {
+        lat: 28,
+        lng: -94
+      },
+      {
+        lat: 27,
+        lng: -93
+      }
+    ],
+    tappable: true,
+    visible: true,
+    color: "red"
+  }
+  map.addPolyline(polylineOptions);
+}
+
+export function onTap(args) {
+  Dialogs.alert("Map tapped.");
+}
+
+export function onPolyline(args) {
+  Dialogs.alert("Polyline tapped.");
 }
